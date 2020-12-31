c6b50ac2c6a982690f25c0c3408bf1a1f7235808
Yoshihide CHUBACHI <yoshi@chubachi.net>
2020-12-31 21:11:34 +0900

    Turn on when google meet is shown

---
+++ background.js
@@ -0,0 +1,20 @@
+// Update the declarative rules on install or upgrade.
+//chrome.runtime.onInstalled.addListener(function () {
+//  console.log("onInstalled called");
+//});
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ color: '#3aa757' }, function () {
+    console.log('The color is green.');
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostEquals: 'meet.google.com' },
+        css: [".z80M1", ".NticYc", ".I3Yihd"]
+      })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
