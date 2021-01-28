288cbcc59fae46b81df5201211f3108018211752
Yoshihide CHUBACHI <yoshi@chubachi.net>
2021-01-28 22:56:53 +0900

    Add webRequest

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+console.log("background.js");
+
 chrome.runtime.onInstalled.addListener(function () {
   console.log('onInstalled called');
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
@@ -11,3 +13,36 @@ chrome.runtime.onInstalled.addListener(function () {
     }]);
   });
 });
+
+/*
+// watch SyncMeetingSpaceCollections and capture request headers
+chrome.webRequest.onSendHeaders.addListener(
+  // callback
+  function (info) {
+    console.log("onSendHeaders");
+    if (info.initiator != 'https://meet.google.com') {
+      console.log('Ignoring CreatingMeetingDevice call from ' + info.initiator);
+      //return {cancel: false};
+    }
+    send_headers = info.requestHeaders;
+  },
+  // filters
+  {
+    urls: [
+      "https://meet.google.com/$rpc/google.rtc.meetings.v1.MeetingSpaceService/SyncMeetingSpaceCollections"
+    ],
+    types: ["xmlhttprequest"]
+  },
+  // opt_extraInfoSpec
+  ["requestHeaders", "extraHeaders"]
+);
+*/
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function (details) {
+    console.log("onBeforeRequest");
+    return { cancel: details.url.indexOf("://www.evil.com/") != -1 };
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking"]
+);
