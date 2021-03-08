ab53b1fa422b5cb6caee47d1e5877386e906b119
Yoshihide CHUBACHI <yoshi@chubachi.net>
2021-03-08 23:23:27 +0900

    Create user interface

--- popup.js
+++ popup.js
@@ -1,5 +1,14 @@
 console.log("popup.js");
 
+chrome.tabs.query({ active: true, currentWindow: true }, (e) => {
+  const current_url = e[0].url;
+  const meet_id = current_url.match("^https://meet.google.com/(.*)$")[1];
+  if (meet_id) {
+    $("#meet_id").text(meet_id);
+  }
+});
+
+// event listeners
 document.getElementById('start_recording').addEventListener('click', start_recording);
 document.getElementById('stop_recording').addEventListener('click', stop_recording);
 
