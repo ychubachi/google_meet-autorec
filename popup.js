044f9a5288f96a430c9d46a09408a49b06fe0b20
Yoshihide CHUBACHI <yoshi@chubachi.net>
2021-03-10 23:09:38 +0900

    enable/disable description

--- popup.js
+++ popup.js
@@ -2,11 +2,19 @@ console.log("popup.js");
 
 chrome.tabs.query({ active: true, currentWindow: true }, (e) => {
   const current_url = e[0].url;
-  const meet_id = current_url.match("^https://meet.google.com/(.*)$")[1];
+  const meet_id = current_url.match("^https://meet.google.com/(.*)$");
   if (meet_id) {
-    $("#meet_id").text(meet_id);
+    $("#meet_id").text(meet_id[1]);
   }
 });
+
+$("#checkbox_autorec").on("change", () => {
+  if ($("#checkbox_autorec").prop("checked")) {
+    $("#textarea_description").prop('disabled', false);
+  } else {
+    $("#textarea_description").prop('disabled', true);
+  }
+})
 
 // event listeners
 document.getElementById('start_recording').addEventListener('click', start_recording);
