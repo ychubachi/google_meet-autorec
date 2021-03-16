3be6397ac6cfc0416d31d30187b85aba8bca5c62
Yoshihide CHUBACHI <yoshi@chubachi.net>
2021-03-16 23:48:47 +0900

    Store url and description

--- popup.js
+++ popup.js
@@ -6,43 +6,83 @@ chrome.tabs.query({ active: true, currentWindow: true }, (e) => {
   current_url = e[0].url;
   const meet_id = current_url.match("^https://meet.google.com/(.*)$");
   if (meet_id) {
-    $("#meet_id").text(meet_id[1]);
+    $("#meet_id").text(`Google Meet: ${meet_id[1]}`);
+  } else {
+    $("#meet_id").text("Not Google Meet page");
+    /*
+    $("#checkbox_autorec").prop("disabled", true);
+    $("#textarea_description").prop("disabled", true);
+    $("#start_recording").prop("disabled", true);
+    $("#stop_recording").prop("disabled", true);
+    */
   }
+
+  get_description_from_list(current_url, function (description) {
+    console.log("description=" + description);
+    if (description) {
+      $("#checkbox_autorec").prop("checked", true);
+      $("#textarea_description").val(description);
+    } else {
+      $("#checkbox_autorec").prop("checked", false);
+      $("#textarea_description").val("");
+    }
+  });
 });
 
 $("#checkbox_autorec").on("change", () => {
   if ($("#checkbox_autorec").prop("checked")) {
     $("#textarea_description").prop('disabled', false);
-    let key = "autorec";
-    let value = current_url;
-    chrome.storage.sync.set({ autorec: value }, function () {
-      console.log('Value is set to ' + value);
-
-      chrome.storage.sync.get(null, function (result) {
-        console.log(result);
-        console.log('Value currently is ' + result[key]);
-      });
-
-    });
+    console.log("description enabled");
   } else {
     $("#textarea_description").prop('disabled', true);
+    console.log("description disabled");
+
+    delete_url_from_list(current_url);
   }
 })
 
 // event listeners
-document.getElementById('start_recording').addEventListener('click', start_recording);
-document.getElementById('stop_recording').addEventListener('click', stop_recording);
+$("#start_recording").on('click', start_recording);
+$("#stop_recording").on('click', stop_recording);
+
+/*
+// Code snipets
+
+// To show all storage data
+chrome.storage.sync.get(null, function (result) {
+  console.log(result);
+});
+
+// To clear all strage data
+chrome.storage.sync.clear();
+*/
 
 function start_recording() {
   console.trace();
-  chrome.runtime.sendMessage(
-    {
-      command: "start_recording"
-    },
-    function (response) {
-      console.log(response);
-    }
-  );
+
+  /*
+  chrome.storage.sync.get(null, function (result) {
+    console.log("old data:")
+    console.log(result);
+  });
+  */
+
+  // Update the auto recording list
+  if ($("#checkbox_autorec").prop("checked")) {
+    const description = $("#textarea_description").val();
+    add_url_to_list(current_url, description);
+  }
+
+  /*
+    chrome.runtime.sendMessage(
+      {
+        command: "start_recording"
+      },
+      function (response) {
+        console.log(response);
+      }
+    );
+    */
 }
 
 function stop_recording() {
@@ -55,4 +95,55 @@ function stop_recording() {
       console.log(response);
     }
   );
+}
+
+
+function add_url_to_list(url, description) {
+  chrome.storage.sync.get("autorec", function (result) {
+    console.log("key=autorec");
+    console.log(result);
+    if (!result["autorec"]) {
+      console.log("create autorec property");
+      result = { autorec: {} };
+    }
+    result.autorec[url] = description;
+    console.log(result.autorec);
+
+    chrome.storage.sync.set({ autorec: result.autorec }, function () {
+      chrome.storage.sync.get(null, function (result) {
+        console.log("new data:");
+        console.log(result);
+      });
+    });
+  });
+}
+
+function get_description_from_list(url, callback) {
+  chrome.storage.sync.get("autorec", function (result) {
+    if (!result["autorec"]) {
+      callback(null);
+    }
+    const description = result.autorec[url];
+    callback(description);
+  });
+}
+
+
+function delete_url_from_list(url) {
+  chrome.storage.sync.get("autorec", function (result) {
+    console.log("key=autorec");
+    console.log(result);
+    if (!result["autorec"]) {
+      return;
+    }
+    delete result.autorec[url];
+    console.log(result.autorec);
+
+    chrome.storage.sync.set({ autorec: result.autorec }, function () {
+      chrome.storage.sync.get(null, function (result) {
+        console.log("new data:");
+        console.log(result);
+      });
+    });
+  });
 }
