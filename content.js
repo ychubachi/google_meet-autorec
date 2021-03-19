de5af000b662a3e6a63846d3bd658538e3a35e0f
Yoshihide CHUBACHI <yoshi@chubachi.net>
2021-03-19 18:28:09 +0900

    Remove dirty console.logs

--- content.js
+++ content.js
@@ -1,8 +1,10 @@
-// There are two process_chrome_message() functions. Why ??
+/**
+ * Listen events from both popup.js and background.js
+ */
 chrome.runtime.onMessage.addListener(
   function (request, sender, sendResponse) {
-    console.trace();
-    console.log(request);
+    // console.trace();
+    // console.log(request);
 
     if (request.command == 'createDevice') {
       create_device(request, sendResponse);
@@ -17,22 +19,34 @@ chrome.runtime.onMessage.addListener(
   }
 );
 
+/**
+ * Check if we can start recoring.
+ * We use magic string to determin it.
+ * The string maybe changed in future Google Meet versions.
+ * 
+ * @returns status
+ */
 function get_status() {
-  console.trace();
+  // console.trace();
   var elems = document.querySelectorAll('div[jsname="CQylAd"]');
-  console.log(elems);
+  // console.log(elems);
 
   if (elems.length > 0) {
     return "can_record";
   } else {
     return "can_not_record";
   }
-
 }
 
-// Request create device so that send back the response to background.js
+/**
+ * Simulate CreateMeetingDevice request again to get the responce form the server.
+ * The responce is send back to background.js to obtain information.
+ * 
+ * @param {*} request 
+ * @param {*} sendResponse 
+ */
 function create_device(request, sendResponse) {
-  console.trace()
+  // console.trace()
 
   var xrequest = new XMLHttpRequest();
   xrequest.withCredentials = true;
@@ -46,30 +60,37 @@ function create_device(request, sendResponse) {
   xrequest.send(request_body);
 
   xrequest.onload = function (e) {
-    console.log('sending response: ' + this.responseText);
+    // console.log('sending response: ' + this.responseText);
     sendResponse({ body: this.responseText });
   };
 }
 
 // Start recording sequence
 function start_recording(request, sendResponse) {
-  console.trace();
+  // console.trace();
+  // console.log("Google Meet AutoRec: Start recording");
   create_meeting_recording(request);
   sendResponse("ok");
 }
 
 // Stop recording
 function stop_recording(request, sendResponse) {
-  console.trace();
+  // console.trace();
+  // console.log("Google Meet AutoRec: Stop recording");
   update_meeting_recording(request, "stop")
   sendResponse("ok");
 }
 
-// Step 1/3: Send CreateMeetingRecording
 var recording_id;
 
+/**
+ * Send CreateMeetingRecording to get recording id.
+ * 
+ * @param {*} request 
+ */
 function create_meeting_recording(request) {
-  console.trace();
+  // console.trace();
+  // console.log("Step 1/3: Send CreateMeetingRecording");
 
   var mrequest = new XMLHttpRequest();
 
@@ -84,19 +105,19 @@ function create_meeting_recording(request) {
     mrequest.setRequestHeader(header.name, header.value);
   })
   var payload = create_meeting_recording_payload("spaces/" + request.space_id);
-  console.log("sending: " + payload);
+  // console.log("sending: " + payload);
   mrequest.send(payload);
 
   mrequest.onload = function (e) {
-    console.trace();
+    // console.trace();
 
-    console.log('start recording response in base64: ' + this.responseText); // this = XMLHttpRequest
-    // response text contains "The conference is gone" on error
+    // console.log('start recording response in base64: ' + this.responseText); // this = XMLHttpRequest
+    // - response text contains "The conference is gone" on error
     var response_str = window.atob(this.responseText)
-    console.log(response_str);
+    // console.log(response_str);
 
     recording_id = response_str.match("Cspaces/.*/recordings/[a-f,0-9,-]*")[0];
-    console.log(recording_id);
+    // console.log("recording_id=" + recording_id);
 
     list_meeting_recording_acks(request);
   };
@@ -130,7 +151,7 @@ function create_meeting_recording_payload(str) {
  * @param {*} request 
  */
 function list_meeting_recording_acks(request) {
-  console.log("list_meeting_recording_acks called");
+  // console.log("Step 2/3: Send listMeetingRecordingAcks");
   var mrequest = new XMLHttpRequest();
 
   mrequest.withCredentials = true;
@@ -144,14 +165,14 @@ function list_meeting_recording_acks(request) {
     mrequest.setRequestHeader(header.name, header.value);
   })
   var payload = list_meeting_recording_acks_payload();
-  console.log("sending: " + payload);
+  // console.log("sending: " + payload);
   mrequest.send(payload);
 
   mrequest.onload = function (e) {
-    console.log('list_meeting_recording_acks response in base64: ');
-    console.log(this.responseText); // TODO this?
+    // console.log('list_meeting_recording_acks response in base64: ');
+    // console.log(this.responseText); // TODO this?
     var response_str = window.atob(this.responseText)
-    console.log(response_str);
+    // console.log(response_str);
 
     update_meeting_recording(request, "start");
   };
@@ -172,11 +193,12 @@ function list_meeting_recording_acks_payload() {
 
 /**
  * Step 3/3: Send updateMeetingRecording
+ * 
  * @param {*} request 
  * @param {*} command 
  */
 function update_meeting_recording(request, command) {
-  console.log("update_meeting_recording called");
+  // console.log("Step 3/3: Send updateMeetingRecording");
   var mrequest = new XMLHttpRequest();
 
   mrequest.withCredentials = true;
@@ -190,14 +212,14 @@ function update_meeting_recording(request, command) {
     mrequest.setRequestHeader(header.name, header.value);
   })
   var payload = update_meeting_recording_payload(command);
-  console.log("sending: " + payload);
+  // console.log("sending: " + payload);
   mrequest.send(payload);
 
   mrequest.onload = function (e) {
-    console.log('update_meeting_recording response in base64: ');
-    console.log(this.responseText);
+    // console.log('update_meeting_recording response in base64: ');
+    // console.log(this.responseText);
     var response_str = window.atob(this.responseText)
-    console.log(response_str);
+    // console.log(response_str);
   };
 }
 
